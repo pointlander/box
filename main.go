72e4d724d88be73d108768084e233ad3ef3d8aba
Andrew Snodgrass <pointlander@gmail.com>
2025-02-26 13:59:29 -0600

    Added model

--- main.go
+++ main.go
@@ -12,13 +12,14 @@ import (
 	"net/http"
 )
 
-func main() {
-	buffer := bytes.NewBuffer([]byte(`{ "model": "llama3.2", "prompt": "Why is the sky blue?"}`))
+// Query submits a query to the llm
+func Query(query string) string {
+	buffer := bytes.NewBuffer([]byte(fmt.Sprintf(`{ "model": "llama3.2", "prompt": "%s"}`, query)))
 	response, err := http.Post("http://10.0.0.54:11434/api/generate", "application/json", buffer)
 	if err != nil {
 		panic(err)
 	}
-	reader := bufio.NewReader(response.Body)
+	reader, answer := bufio.NewReader(response.Body), ""
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
@@ -27,7 +28,47 @@ func main() {
 		data := map[string]interface{}{}
 		err = json.Unmarshal([]byte(line), &data)
 		text := data["response"].(string)
-		fmt.Printf(text)
+		answer += text
 	}
-	fmt.Println()
+	return answer
+}
+
+func main() {
+	type Vector struct {
+		Vector *[256]float32
+		Symbol byte
+	}
+	mind, index := make([]Vector, 1024), 0
+	m := NewMixer()
+	m.Add(0)
+	query := "Why is the sky blue?"
+	for {
+		answer := Query(query)
+		fmt.Printf(answer)
+		fmt.Println("\n----------------------------------------")
+		for _, v := range []byte(answer) {
+			index = (index + 1) % len(mind)
+			mind[index].Vector = m.Mix()
+			mind[index].Symbol = v
+			m.Add(v)
+		}
+		query := ""
+		for i := 0; i < 128; i++ {
+			q := m.Mix()
+			max, symbol := float32(0.0), byte(0)
+			for _, v := range mind {
+				if v.Symbol == 0 {
+					continue
+				}
+				cs := CS(v.Vector[:], q[:])
+				if cs > max {
+					max, symbol = cs, v.Symbol
+				}
+			}
+			query += fmt.Sprintf("%c", symbol)
+			m.Add(symbol)
+		}
+		fmt.Printf(query)
+		fmt.Println("\n----------------------------------------")
+	}
 }
