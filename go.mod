cef9893e411c6e4e6e087ef21e8ba55900fd4f94
Andrew Snodgrass <pointlander@gmail.com>
2025-03-05 13:37:02 -0600

    WIP neural network

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/pointlander/box
 
 go 1.23.3
 
-require github.com/alixaxel/pagerank v0.0.0-20200105181019-900657b89dcb // indirect
+require (
+	github.com/alixaxel/pagerank v0.0.0-20200105181019-900657b89dcb // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/pointlander/gradient v0.0.0-20240226214843-e3d2a19564fd // indirect
+	github.com/ziutek/blas v0.0.0-20190227122918-da4ca23e90bb // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -9,9 +9,32 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"math"
+	"math/rand"
 	"net/http"
+	"strings"
+
+	"github.com/pointlander/gradient/tf32"
 
 	"github.com/alixaxel/pagerank"
+)
+
+const (
+	// B1 exponential decay of the rate for the first moment estimates
+	B1 = 0.8
+	// B2 exponential decay rate for the second-moment estimates
+	B2 = 0.89
+	// Eta is the learning rate
+	Eta = 1.0e-5
+)
+
+const (
+	// StateM is the state for the mean
+	StateM = iota
+	// StateV is the state for the variance
+	StateV
+	// StateTotal is the total number of states
+	StateTotal
 )
 
 // Prompt is a llm prompt
@@ -77,7 +100,11 @@ func main() {
 	query := "Why is time symmetric at quantum scales by asymmetric at large scales?"
 	for {
 		answer := Query(query)
+		others := tf32.NewSet()
+		others.Add("input", 8*256, len([]byte(answer)))
+		others.Add("output", 256, len([]byte(answer)))
 		if len(answer) != 0 {
+			input, output := others.ByName["input"], others.ByName["output"]
 			fmt.Printf(answer)
 			fmt.Println("\n----------------------------------------")
 			for _, v := range []byte(answer) {
@@ -85,10 +112,43 @@ func main() {
 				vectors := m.Mix()
 				mind[index].Vector = vectors.Data
 				mind[index].Symbol = v
+				input.X = append(input.X, vectors.Data...)
+				out := make([]float32, 256)
+				out[v] = 1
+				output.X = append(output.X, out...)
 				m.Add(v)
 			}
 		}
-		fmt.Println("half way there")
+
+		rng := rand.New(rand.NewSource(1))
+		s := make(map[byte]bool)
+		for _, v := range []byte(answer) {
+			s[v] = true
+		}
+		set := tf32.NewSet()
+		set.Add("w", 8*256, len(s))
+		set.Add("b", len(s))
+		for i := range set.Weights {
+			w := set.Weights[i]
+			if strings.HasPrefix(w.N, "b") {
+				w.X = w.X[:cap(w.X)]
+				w.States = make([][]float32, StateTotal)
+				for i := range w.States {
+					w.States[i] = make([]float32, len(w.X))
+				}
+				continue
+			}
+			factor := math.Sqrt(2.0 / float64(w.S[0]))
+			for i := 0; i < cap(w.X); i++ {
+				w.X = append(w.X, float32(rng.NormFloat64()*factor))
+			}
+			w.States = make([][]float32, StateTotal)
+			for i := range w.States {
+				w.States[i] = make([]float32, len(w.X))
+			}
+		}
+
+		fmt.Println("done training")
 		query = ""
 		i := 0
 		for {
